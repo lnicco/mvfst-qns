d400784bed517d08e7c21c1aa066e6711c4ffa7a
Luca Niccolini <lniccolini@fb.com>
2019-07-27 16:20:14 -0700

    mvfst+proxygen Docker image for use with the quic-network-simulator

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM martenseemann/quic-network-simulator-endpoint:latest
+
+# Get and build proxygen with HTTP/3 support
+RUN apt-get --yes update
+RUN apt-get install --yes git sudo libdouble-conversion-dev
+RUN git clone https://github.com/facebook/proxygen.git
+RUN cd proxygen/proxygen && ./build.sh -q
+
+# copy run script and run it
+COPY run_endpoint.sh .
+RUN chmod +x run_endpoint.sh
+ENTRYPOINT [ "./run_endpoint.sh" ]
---
+++ run_endpoint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+HQ_CLI=/proxygen/proxygen/_build/proxygen/httpserver/hq
+# Set up the routing needed for the simulation
+/setup.sh
+
+ROLE=$1
+shift
+
+if [ "$ROLE" == "client" ]; then
+    echo "Starting QUIC client..."
+    ${HQ_CLI} --mode=client --host=server --use_draft=true --draft_version=22 --protocol=h3-22 --port=4433 --path=$(yes '/10000000' | head -n 10 | paste -sd',') --conn_flow_control=1048576 --stream_flow_control=256000 "$@"
+
+elif [ "$ROLE" == "server" ]; then
+    echo "Running QUIC server on 0.0.0.0:4433"
+    mdkir /logs
+    ${HQ_CLI} --mode=server --port=4433 --h2port=4434 --use_draft=true --draft-version=22 --logdir=/logs --host=0.0.0.0 --v=0 "$@"
+fi
