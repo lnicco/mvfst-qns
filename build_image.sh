17a74c7aa34ab3805959e5679d64f8d61f14840a
Luca Niccolini <lniccolini@fb.com>
2020-01-23 10:48:32 -0800

    script to build image

---
+++ build_image.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+# use this to build the image from latest master
+# NOTE: it clones the repo from scratch
+
+set -e
+set -o nounset
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+mkdir -p ${DIR}/auto/logs
+
+cd ${DIR}/auto/
+rm -rf mvfst-qns || true
+git clone https://github.com/lnicco/mvfst-qns.git
+cd mvfst-qns
+
+DATE=$(date +"%Y%m%d_%H%M%S")
+IMAGEHASH=$(sudo docker build . | tee ../logs/build.${DATE} | grep "Successfully built" | cut -d " " -f 3)
+if [ -z $IMAGEHASH ]; then
+    echo "Image build failed or not necessary: check logs/build.${DATE}"
+else
+    sudo docker tag ${IMAGEHASH} lnicco/mvfst-qns:latest
+    sudo docker push lnicco/mvfst-qns
+fi
+
